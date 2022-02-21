11aeda9382abbb5ebd0c41d8a42bbc948925d711
Sukhar <ssuxxarr@gmail.com>
2022-02-21 23:17:21 +0300

    remove unnecessary files

--- long_redirect.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.get("https://playground.learnqa.ru/api/long_redirect")
-redirects = len(response.history)
-
-print(f"Количество редиректов : {redirects}")
-print(f"Итоговый адрес: {response.url}")
--- test_input_phrase.py
+++
@@ -1,4 +0,0 @@
-class TestInputPhrase:
-    def test_input_phrase(self):
-        phrase = input("Set a phrase")
-        assert len(phrase) < 15, f"The phrase '{phrase}' is longer than 15 characters"
